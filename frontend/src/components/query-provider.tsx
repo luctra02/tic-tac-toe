12ed7552f6bae7b7833befd7a0159dbc4b7092d9
Lucas Tran <lucastran1107@gmail.com>
2024-12-19 17:17:51 +0100

    Revert "feat(frontend): :sparkles: handle disconnect during a game"
    This reverts commit e2d9b26d0fc89d628080cdeee988e8547d7230ff.

--- frontend/src/components/query-provider.tsx
+++ frontend/src/components/query-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode, useState } from "react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
     const [queryClient] = useState(
